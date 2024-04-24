d1e25df59c1e4442f30800a85db4b9b2d2390c48
vbrunoro <vfmbrunoro@gmail.com>
2024-04-24 15:35:34 -0300

    Add api requests and transactions context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -25,7 +25,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>{' '}
+        <strong>R$ 17.400,00</strong>
       </SummaryCard>
     </SummaryContainer>
   )
